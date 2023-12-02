1d2d09ceb9cfe663f5d9ef403b7b6f6271d00385
Rohit Bhatkulkar <rohitbhatkulkar.19@gmail.com>
2023-12-02 09:14:34 +0530

    30oct assignments

---
+++ pythonlearning1x/30th Oct 2023/Lab014.py
@@ -0,0 +1,19 @@
+#Intersection and union of two sets.
+
+# tup1=(6,-2,3,4,40)
+# max = 0
+# min =10
+#
+# for i in tup1:
+#    if max < i:
+#        max=i
+#
+# print("Max Number is", max)
+
+#Find the intersection and union of two sets.
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 5, 6, 7}
+
+
+

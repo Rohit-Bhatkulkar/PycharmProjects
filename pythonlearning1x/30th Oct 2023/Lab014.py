f0b3104d8ada7f9885baf9e3ad72ab07c846cbe1
Rohit Bhatkulkar <rohitbhatkulkar.19@gmail.com>
2023-12-12 10:39:25 +0530

    30oct assignments

--- pythonlearning1x/30th Oct 2023/Lab014.py
+++ pythonlearning1x/30th Oct 2023/Lab014.py
@@ -1,4 +1,4 @@
-#Intersection and union of two sets.
+# Intersection and union of two sets.
 
 # tup1=(6,-2,3,4,40)
 # max = 0
@@ -10,10 +10,31 @@
 #
 # print("Max Number is", max)
 
-#Find the intersection and union of two sets.
+# Find the intersection and union of two sets.
 
-set1 = {1, 2, 3, 4, 5}
-set2 = {3, 4, 5, 6, 7}
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4, 5, 6, 7}
+#
+# setuni = set1.union(set2)
+# print(setuni)
 
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4, 5, 6, 7}
+
+# setuni = set1.intersection(set2)
+# print(setuni)
+
+# Remove duplicate elements from a list.
+
+# List1 = [ 2, 2, 3, 3, 4, 5]
+
+# Remove_Dup = set(List1)
+# set.union(Remove_Dup)
+# print(Remove_Dup)
+
+#Remove a key-value pair from the dictionary.
+
+my_dict = dict(Batman=1234,Age=123,Phone= 979867857)
+print(my_dict.pop('Phone'))
 
 

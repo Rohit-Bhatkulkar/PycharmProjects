4f3b528b7b76eb9231fc65646c17fea0ce79c065
Rohit Bhatkulkar <rohitbhatkulkar.19@gmail.com>
2024-01-11 12:23:58 +0530

    Update Lab013.py
    removed commented code

--- pythonlearning1x/27thOct 2023/Lab013.py
+++ pythonlearning1x/27thOct 2023/Lab013.py
@@ -1,50 +1,50 @@
 
 #Write a Python program to find the largest number in a list.
 # def largestnumber(number):
-#
-#     Biggestnumber= number[0]
-#
-#     for num in number:
-#         if num> Biggestnumber:
-#             Biggestnumber = num
-#
-#
-#     return Biggestnumber
-#
-# print(largestnumber([1,2,8,10]))
+
+   Biggestnumber= number[0]
+
+    for num in number:
+        if num> Biggestnumber:
+            Biggestnumber = num
+
+
+    return Biggestnumber
+
+ print(largestnumber([1,2,8,10]))
 
 #Write a Python program to find the smallest number in a list.
 
-# def smallestnumber(number):
-#
-#     smallnumber=number[0]
-#
-#     for num in number:
-#         if num < smallnumber:
-#             smallnumber=num
-#     return smallnumber
-#
-# output=smallestnumber([1,234,0,687632786,3763743])
-# print(output)
+ def smallestnumber(number):
+
+    smallnumber=number[0]
+
+    for num in number:
+        if num < smallnumber:
+          smallnumber=num
+    return smallnumber
+
+ output=smallestnumber([1,234,0,687632786,3763743])
+ print(output)
 
 # Write a Python program to sum all numbers in a list.
-# def sumoflist(list1):
-#
-#     intialsum = 0
-#     for i in list1:
-#         intialsum = intialsum + i
-#
-#     return intialsum
-# print(sumoflist([1,2,3]))
+ def sumoflist(list1):
+
+    intialsum = 0
+     for i in list1:
+         intialsum = intialsum + i
+
+     return intialsum
+ print(sumoflist([1,2,3]))
 
 # Write a Python program to multiply all numbers in a list.
-# def list_mul(Multiply):
-#     Intial_result = 1
-#     for i in Multiply:
-#         Intial_result = Intial_result * i
-#     return Intial_result
-#
-# print(list_mul([1, 2, 3,4]))
+ def list_mul(Multiply):
+    Intial_result = 1
+    for i in Multiply:
+        Intial_result = Intial_result * i
+    return Intial_result
+
+ print(list_mul([1, 2, 3,4]))
 
 #Write a Python program to count the number of strings in a list
 #where the string length is 2 or more and the first and last character are the same.
